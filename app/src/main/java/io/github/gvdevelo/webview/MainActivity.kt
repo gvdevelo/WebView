bc33e4e955e520fa2aaf269d1a7a54320e5142b8
gvdevelo <gvdevelo@users.noreply.github.com>
2020-05-27 21:58:51 +0200

    Initial commit

---
+++ app/src/main/java/io/github/gvdevelo/webview/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.gvdevelo.webview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
