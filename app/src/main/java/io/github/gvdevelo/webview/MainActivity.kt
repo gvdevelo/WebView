d1d7feb811103197c3c4b15f05f59479ea8f0566
gvdevelo <gvdevelo@users.noreply.github.com>
2020-05-28 20:18:48 +0200

    prova

--- app/src/main/java/io/github/gvdevelo/webview/MainActivity.kt
+++ app/src/main/java/io/github/gvdevelo/webview/MainActivity.kt
@@ -2,11 +2,20 @@ package io.github.gvdevelo.webview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.mywebview)
+        val myurl = "http://www.bnl.it"
+        // myWebView.webViewClient = WebViewClient()
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.loadUrl(myurl)
+
     }
 }
